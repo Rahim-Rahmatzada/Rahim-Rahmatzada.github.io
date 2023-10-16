31071b8ebddac7bb4bb969d01a754880898c127c
Rahim-Rahmatzada <abdul.rahmatzada@city.ac.uk>
2023-10-16 13:16:48 +0100

    Re-design everything

---
+++ Main.js
@@ -0,0 +1,66 @@
+let i = 0;
+let isDeleting = false;
+const text = 'Rahim Rahmatzada';
+const speed = 150;
+
+function typeWriter() {
+    document.getElementById("name").innerHTML = text.slice(0, i);
+
+    if (i === text.length && !isDeleting) {
+        setTimeout(() => { isDeleting = true; typeWriter(); }, 500);  // Adding a pause
+        return;  // Ensure we exit here to avoid continuous execution
+    } else if (i === 0 && isDeleting) {
+        isDeleting = false;
+    }
+
+    const typeSpeed = isDeleting ? speed * 0.5 : speed;
+
+    if (isDeleting) {
+        i--;
+    } else {
+        i++;
+    }
+    setTimeout(typeWriter, typeSpeed);
+}
+
+document.addEventListener("DOMContentLoaded", typeWriter);
+
+
+
+function animateTechIcons() {
+    document.querySelector(".tech-icon.lua").style.animation = "rollFromLeft 2s forwards";
+    document.querySelector(".tech-icon.java").style.animation = "rollFromLeft 2s forwards";
+    document.querySelector(".tech-icon.python").style.animation = "rollFromRight 2s forwards";
+    document.querySelector(".tech-icon.sql").style.animation = "rollFromRight 2s forwards";
+}
+
+
+const aboutSection = document.querySelector('#about');
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+let hasAnimated = false;
+
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !hasAnimated) {
+            animateTechIcons();
+            hasAnimated = true;
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+observer.observe(aboutSection);
+
+
+document.querySelector('#menu a[href="#about"]').addEventListener('click', () => {
+    if (!hasAnimated) {
+        setTimeout(animateTechIcons, 300); // Give it a slight delay to allow for scrolling
+        hasAnimated = true;
+    }
+});
---
+++ SquareAnimation.js
@@ -0,0 +1,65 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const introSection = document.getElementById("intro");
+//
+//     for (let i = 0; i < 10; i++) {
+//         const square = document.createElement("div");
+//         square.className = "square";
+//
+//         square.style.left = Math.random() * introSection.clientWidth + "px";
+//         square.style.top = Math.random() * introSection.clientHeight + "px";
+//
+//         introSection.appendChild(square);
+//
+//         setInterval(() => {
+//             // Update position at intervals
+//             square.style.left = Math.random() * introSection.clientWidth + "px";
+//             square.style.top = Math.random() * introSection.clientHeight + "px";
+//         }, 3000); // Interval to sync with fadeInOut animation
+//     }
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const introSection = document.getElementById("intro");
+    const imageElement = introSection.querySelector("img");
+
+    function getRandomPosition(elementWidth, elementHeight) {
+        let xPos = Math.random() * introSection.clientWidth;
+        let yPos = Math.random() * introSection.clientHeight;
+
+        // Get image dimensions and position
+        const imageRect = imageElement.getBoundingClientRect();
+
+        // Check if the random position is over the image and re-roll if it is
+        while (
+            xPos > imageRect.left &&
+            xPos < imageRect.right &&
+            yPos > imageRect.top &&
+            yPos < imageRect.bottom
+            ) {
+            xPos = Math.random() * introSection.clientWidth;
+            yPos = Math.random() * introSection.clientHeight;
+        }
+
+        return { x: xPos, y: yPos };
+    }
+
+    for (let i = 0; i < 10; i++) {
+        // Use setTimeout to delay the execution of the function for each square
+        setTimeout(() => {
+            const square = document.createElement("div");
+            square.className = "square";
+
+            const { x, y } = getRandomPosition(15, 15); // Assume squares are 15x15px
+            square.style.left = x + "px";
+            square.style.top = y + "px";
+
+            introSection.appendChild(square);
+
+            setInterval(() => {
+                const { x, y } = getRandomPosition(15, 15);
+                square.style.left = x + "px";
+                square.style.top = y + "px";
+            }, 3000); // 3000ms to sync with the fadeInOut animation duration
+        }, i * 500); // Introduce a delay of 500ms times the index (i)
+    }
+});
